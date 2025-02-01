88e77c93ffb0ed3c0abb1e2a041f48e74d5d0d05
shubhamkunwar10 <fonders@metakul.com>
2025-02-01 20:56:23 +0530

    add cors

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import config from "./config";
 // Importing modules from different folders
 import { initializeRootAdmin, backendApi } from "./backend";
 import { authApi } from "./auth";
+import cors from 'cors';
 
 const app = express();
 const { port, env, rootAdmin, rootPassword } = config;
@@ -13,8 +14,15 @@ const { port, env, rootAdmin, rootPassword } = config;
 // Middleware for parsing JSON
 app.use(bodyParser.json());
 
+// CORS configuration
+app.use(cors({
+    origin: '*', // or specify the domain explicitly
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'boundary'],
+  }));
+
 // Mount APIs
-app.use("/backend", backendApi);
+app.use("/backend/v1", backendApi);
 app.use("/auth", authApi);
 
 // Initialize Admin
